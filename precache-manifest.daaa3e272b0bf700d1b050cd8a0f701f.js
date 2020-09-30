5338f1ee6ab30dd4ef0752cd71a802e1618eb2cb
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2020-09-29 22:35:58 -0500

    Updates

---
+++ precache-manifest.daaa3e272b0bf700d1b050cd8a0f701f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba3cf644e1b5a6bf9c36af80371e1aa",
+    "url": "/memorama-pokemon/index.html"
+  },
+  {
+    "revision": "869fd902c70d1c6548d2",
+    "url": "/memorama-pokemon/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "2cd3e330752e28818f63",
+    "url": "/memorama-pokemon/static/css/main.d7575b90.chunk.css"
+  },
+  {
+    "revision": "869fd902c70d1c6548d2",
+    "url": "/memorama-pokemon/static/js/2.5b77732d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/memorama-pokemon/static/js/2.5b77732d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd3e330752e28818f63",
+    "url": "/memorama-pokemon/static/js/main.ff1dcd35.chunk.js"
+  },
+  {
+    "revision": "6178479d6cc4e349a90e",
+    "url": "/memorama-pokemon/static/js/runtime-main.455e50e2.js"
+  },
+  {
+    "revision": "2b4798f5c303bb0d9001413af970a22b",
+    "url": "/memorama-pokemon/static/media/pokebola.2b4798f5.svg"
+  }
+]);
